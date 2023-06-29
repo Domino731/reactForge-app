0cc58e70990f3f6b9b5a24373f768ebaec715d5c
Domino731 <d.orzechowski18@gmail.com>
2023-06-29 18:49:20 +0200

    feature/project setup/ packages up

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import { Application, Loader, Texture, AnimatedSprite } from "pixi.js";
-import { getSpine } from "./spine-example";
+import { Application } from "pixi.js";
 import "./style.css";
 
 declare const VERSION: string;
@@ -15,66 +14,4 @@ const app = new Application({
     height: gameHeight,
 });
 
-window.onload = async (): Promise<void> => {
-    await loadGameAssets();
-
-    document.body.appendChild(app.view);
-
-    resizeCanvas();
-
-    const birdFromSprite = getBird();
-    birdFromSprite.anchor.set(0.5, 0.5);
-    birdFromSprite.position.set(gameWidth / 2, 530);
-
-    const spineExample = getSpine();
-    spineExample.position.y = 580;
-
-    app.stage.addChild(birdFromSprite);
-    app.stage.addChild(spineExample);
-    app.stage.interactive = true;
-};
-
-async function loadGameAssets(): Promise<void> {
-    return new Promise((res, rej) => {
-        const loader = Loader.shared;
-        loader.add("rabbit", "./assets/simpleSpriteSheet.json");
-        loader.add("pixie", "./assets/spine-assets/pixie.json");
-
-        loader.onComplete.once(() => {
-            res();
-        });
-
-        loader.onError.once(() => {
-            rej();
-        });
-
-        loader.load();
-    });
-}
-
-function resizeCanvas(): void {
-    const resize = () => {
-        app.renderer.resize(window.innerWidth, window.innerHeight);
-        app.stage.scale.x = window.innerWidth / gameWidth;
-        app.stage.scale.y = window.innerHeight / gameHeight;
-    };
-
-    resize();
-
-    window.addEventListener("resize", resize);
-}
-
-function getBird(): AnimatedSprite {
-    const bird = new AnimatedSprite([
-        Texture.from("birdUp.png"),
-        Texture.from("birdMiddle.png"),
-        Texture.from("birdDown.png"),
-    ]);
-
-    bird.loop = true;
-    bird.animationSpeed = 0.1;
-    bird.play();
-    bird.scale.set(3);
-
-    return bird;
-}
+window.onload = async (): Promise<void> => {};

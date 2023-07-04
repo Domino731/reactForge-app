b872f22f13e8bfaf7b18d10790314228f77b502e
Domino731 <d.orzechowski18@gmail.com>
2023-07-04 17:36:07 +0200

    feature - theme basic colors

---
+++ src/constants/theme.ts
@@ -0,0 +1,9 @@
+/** GLOBAL CONSTANTS, do not change anyning  */
+export const THEME = {
+    grayFirst: "#212529",
+    graySecond: "#343a40",
+    grayThird: "#495057",
+    grayFourth: "#6c757d",
+    grayFifth: "#adb5bd",
+    bluePrintPanel: "#333333",
+};
